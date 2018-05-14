6f30002f7017f1abda2c00998e3ea1955080e461
Maximiliano Mercado <31021243+maximilianonahuelmercado@users.noreply.github.com>
2018-05-13 22:16:02 -0300

    Delete EstadoSoldado.java

--- Soldado/src/guerra/EstadoSoldado.java
+++
@@ -1,9 +0,0 @@
-package guerra;
-
-public interface EstadoSoldado {
-	
-	public void recibirDisparo(Soldado soldado);
-	public void recibirCuracion(Soldado soldado);
-	public String informarEstado();
-	
-}
