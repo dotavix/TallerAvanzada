0f41932dec60d4ff11141ea3390f3308bc2f092e
Maximiliano Mercado <maximilianomercado.n@gmail.com>
2018-06-23 00:12:37 -0300

    Mejoras a la ventana de Login con validaciones

--- JohnVonNeumann/src/main/java/frame/Login.java
+++ JohnVonNeumann/src/main/java/frame/Login.java
@@ -14,14 +14,11 @@ import java.awt.event.ActionEvent;
 import bdd.*;
 import java.awt.Font;
 
-
-
-public class Login{
+public class Login {
 
 	JFrame frmChatbot;
 	private JTextField usuarioTxtField;
 	private JTextField passTxtField;
-
 
 	/**
 	 * Launch the application.
@@ -44,7 +41,7 @@ public class Login{
 	 */
 	public Login() {
 		initialize();
-	
+
 	}
 
 	/**
@@ -56,63 +53,68 @@ public class Login{
 		frmChatbot.setBounds(100, 100, 450, 300);
 		frmChatbot.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmChatbot.getContentPane().setLayout(null);
-		
+
 		usuarioTxtField = new JTextField();
 		usuarioTxtField.setBounds(140, 45, 170, 22);
 		frmChatbot.getContentPane().add(usuarioTxtField);
 		usuarioTxtField.setColumns(10);
-		
+
 		passTxtField = new JPasswordField();
 		passTxtField.setBounds(140, 94, 170, 22);
 		frmChatbot.getContentPane().add(passTxtField);
 		passTxtField.setColumns(10);
-		
+
 		JLabel lblUsuario = new JLabel("Usuario");
 		lblUsuario.setFont(new Font("Consolas", Font.PLAIN, 15));
 		lblUsuario.setBounds(43, 48, 85, 16);
 		frmChatbot.getContentPane().add(lblUsuario);
-		
+
 		JLabel lblContrasea = new JLabel("Contrase\u00F1a");
 		lblContrasea.setFont(new Font("Consolas", Font.PLAIN, 15));
 		lblContrasea.setBounds(43, 97, 85, 16);
 		frmChatbot.getContentPane().add(lblContrasea);
-		
+
 		JButton btnConectarse = new JButton("Conectarse");
 		btnConectarse.setFont(new Font("Consolas", Font.PLAIN, 15));
 		btnConectarse.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-			String usr = usuarioTxtField.getText();
-			String pass = passTxtField.getText();
-			Conector con = new Conector();
-			con.connect();
-			if(usuarioTxtField.getText().isEmpty() || passTxtField.getText().isEmpty() )
-				JOptionPane.showMessageDialog(null, "Todos los campos son obligatorios", "ERROR - Empty fields",
-						JOptionPane.ERROR_MESSAGE);
-			else {
-				if(con.mostrarUsuario(usr))
-					System.out.println("Adentro");
+				String usr = usuarioTxtField.getText();
+				String pass = passTxtField.getText();
+				Conector con = new Conector();
+				con.connect();
+				if (usuarioTxtField.getText().isEmpty() || passTxtField.getText().isEmpty())
+					JOptionPane.showMessageDialog(null, "Todos los campos son obligatorios", "ERROR - Empty fields",
+							JOptionPane.ERROR_MESSAGE);
 				else {
-					JOptionPane.showMessageDialog(null, "Mira capo, me parece que no te conece nadie. Registrate y no me hagas perder mas el tiempo en esta consulta. Saludos y cuidate pa.", "Usuario inexistente", JOptionPane.ERROR_MESSAGE);
+					if (con.mostrarUsuario(usr) == false) {
+
+						JOptionPane.showMessageDialog(null,
+								"Mira capo, me parece que no te conece nadie. Registrate y no me hagas perder mas el tiempo en esta consulta. Saludos y cuidate pa.",
+								"Usuario inexistente", JOptionPane.ERROR_MESSAGE);
+					} else if (con.mostrarUsuario(usr) && (con.mostrarPassword(pass) == false)) {
+
+						JOptionPane.showMessageDialog(null, "La contraseña no es valida", "Contraseña incorrecta",
+								JOptionPane.ERROR_MESSAGE);
+					} else {
+						System.out.println("Adentro");
+					}
 				}
-			}
 				con.close();
 			}
-			
+
 		});
 		btnConectarse.setBounds(64, 162, 125, 36);
 		frmChatbot.getContentPane().add(btnConectarse);
-		
+
 		JButton btnNewButton = new JButton("Registrarse");
 		btnNewButton.setFont(new Font("Consolas", Font.PLAIN, 15));
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				
+
 				Registrarse reg = new Registrarse();
 				reg.frmRegistro.setVisible(true);
 				frmChatbot.dispose();
-				
-				
-				
+
 			}
 		});
 		btnNewButton.setBounds(213, 162, 148, 36);
